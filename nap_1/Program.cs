4042c7e32aaa80ad09d7be02ff21f553ef50ad82
szajberpiraty <szajberpiraty@gmail.com>
2018-08-22 17:17:39 +0200

    Start
    Alap konzolos megoldás a sharp develop-al

---
+++ nap_1/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Készítette a SharpDevelop.
+ * Felhasználó: razgon
+ * Dátum: 2018. 08. 22.
+ * Idő: 17:16
+ * 
+ * A sablon megváltoztatásához használja az Eszközök | Beállítások | Kódolás | Szabvány Fejlécek Szerkesztését.
+ */
+using System;
+
+namespace nap_1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello Tanfolyam!");
+									
+			Console.ReadKey(true);
+		}
+	}
+}
