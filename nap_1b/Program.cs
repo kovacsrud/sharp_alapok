1e6591a19fce9d45b201a1c799c2a7f6e35a0e28
szajberpiraty <szajberpiraty@gmail.com>
2018-08-22 18:00:34 +0200

    #2
    tömbök

---
+++ nap_1b/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Készítette a SharpDevelop.
+ * Felhasználó: razgon
+ * Dátum: 2018. 08. 22.
+ * Idő: 17:51
+ * 
+ * A sablon megváltoztatásához használja az Eszközök | Beállítások | Kódolás | Szabvány Fejlécek Szerkesztését.
+ */
+using System;
+
+namespace nap_1b
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			//összetettebb adatszerkezetek
+			//tömb
+			//10 elemű tömb, int típusú elemekkel
+			int[] tomb1=new int[10];
+			
+			//20 elemű, karaktereket tartalmazó tömb
+			char[] tomb2=new char[20];
+			
+			//értékadás
+			tomb1[0]=118;
+			tomb2[0]='g';
+			
+			
+			
+			Console.ReadLine();
+		}
+	}
+}
